7c9b4568985fe0114d706703ffaf205d29314e31
GeorgeFlood <mrgeorgeflood@gmail.com>
2020-12-23 13:17:04 +0000

    js h1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-function on() {
-  document.getElementsByClassName("overlay").style.display = "block";
+const text = document.querySelector('.text');
+const stringtext = text.textContent;
+const splitText = stringtext.split('');
+text.textContent = '';
+for(let i = 0; i < splitText.length; i++){
+	text.innerHTML += '<span>' + splitText[i] + '</span>';
 }
 
-console.log('hello');
+let char = 0;
+let timer = setInterval(onTick, 150);
+
+function onTick(){
+	const span = text.querySelectorAll('span')[char];
+	span.classList.add('jsfade');
+	char++
+	if(char === splitText.length){
+		complete();
+		return;
+	}
+}
+
+function complete(){
+	clearInterval(timer);
+	timer = null;
+}
